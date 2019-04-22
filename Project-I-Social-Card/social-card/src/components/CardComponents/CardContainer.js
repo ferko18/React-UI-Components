40f463b3b38641fa6551053eb21c3ee9e41b93c8
Firealem Erko <firealemg@yahoo.com>
2019-04-22 17:09:30 -0400

    added cardcontainer and cardbanner components

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+import CardBanner from './CardBanner'
+
+
+const CardContainer = () => {
+    return (
+      <div className='CardContainer'>
+      <CardBanner/>
+      
+      </div>
+    )
+      
+    
+  };
+  
+  export default CardContainer;
