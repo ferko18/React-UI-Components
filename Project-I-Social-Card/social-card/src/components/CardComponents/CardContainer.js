eb624a52a2b8a7f6e3a008585d3b412967ab3277
Firealem Erko <firealemg@yahoo.com>
2019-04-22 17:58:21 -0400

    end of MVP

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import './Card.css';
 import CardBanner from './CardBanner'
-
+import CardContent from './CardContent'
 
 const CardContainer = () => {
     return (
-      <div className='CardContainer'>
+        <a href = "https://www.reactjs.org">
+        
+        <div className='CardContainer'>
       <CardBanner/>
+      <CardContent/>
       
       </div>
+        
+        </a>
+      
     )
       
     
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+const CardContent = () => {
+    return (
+      <div className='CardContent'>
+        <h4>Get Started With React </h4>
+        <p>React makes it painless to create interactive UIs. Design simple views for each state in your application.</p>
+        <footer>reactjs.org</footer>
+              
+      </div>
+    )
+      
+    
+  };
+  
+  export default CardContent;
