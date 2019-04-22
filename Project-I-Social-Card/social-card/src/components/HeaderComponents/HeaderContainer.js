d812bb76beaa919a927fc9e272da1f2d1deab620
Firealem Erko <firealemg@yahoo.com>
2019-04-22 16:24:27 -0400

    added headercontainer , imagethumbnail and headercontent components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Header.css';
+import ImageThumbnail from './ImageThumbnail'
+import HeaderContent from './HeaderContent'
+
+const HeaderContainer = () => {
+    return (
+      <div className='HeaderContainerp'>
+      <ImageThumbnail/>
+      <HeaderContent/>
+      
+      </div>
+    )
+      
+    
+  };
+  
+  export default HeaderContainer;
