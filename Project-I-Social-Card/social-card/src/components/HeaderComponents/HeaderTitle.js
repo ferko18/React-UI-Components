857b2d6ff8b3fdc08313d16cb7b5aad9e9e2b109
Firealem Erko <firealemg@yahoo.com>
2019-04-22 16:52:54 -0400

    added HeadeTitle component

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,15 @@
-import React from 'react';
-import './Header.css';
+import React from "react";
+import "./Header.css";
+
+const HeaderTitle = () => {
+  return (
+    <div className="HeaderTitle">
+     
+        <h4>Lambda School</h4>
+        <h5>@LambdaSchool . 26 jan</h5>
+      
+    </div>
+  );
+};
+
+export default HeaderTitle;
